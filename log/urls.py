fa3b53ab6f60d7de83a0bd1dea9433d1b3519d7e
tlss94 <doantl89@gmail.com>
2017-03-14 19:28:48 -0400

    ll

--- log/urls.py
+++ log/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
 
 #from datetime import datetime
 #from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
   
