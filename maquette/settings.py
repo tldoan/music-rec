974d7c65cce4f6e345f5bd4f7c74e8f3523df5bf
tlss94 <doantl89@gmail.com>
2017-03-14 15:05:36 -0400

    emplacement databse

--- maquette/settings.py
+++ maquette/settings.py
@@ -113,8 +113,7 @@ DATABASES = {
 }
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
@@ -150,6 +149,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
