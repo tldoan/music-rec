4937ffe134bad3d97cc250cca869e8d3422fe7be
tlss94 <doantl89@gmail.com>
2017-03-15 14:38:28 -0400

    ll

--- maquette/settings.py
+++ maquette/settings.py
@@ -90,8 +90,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'maquette.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
