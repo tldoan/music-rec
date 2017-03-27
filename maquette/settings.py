2f282c1f66c1e80433e10efe9c4ad9d295ba3f64
tlss94 <doantl89@gmail.com>
2017-03-27 21:13:11 +0100

    d

--- maquette/settings.py
+++ maquette/settings.py
@@ -97,7 +97,12 @@ WSGI_APPLICATION = 'maquette.wsgi.application'
 
 
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 import dj_database_url
