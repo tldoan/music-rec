a39716c49658b60534cd02139346cb9281ab19b2
tlss94 <doantl89@gmail.com>
2017-05-09 16:14:33 -0400

    debug=false

--- maquette/settings.py
+++ maquette/settings.py
@@ -29,7 +29,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '5=87fsk=@y5^!cd70yr=!kti!2vw+8prmx)l7z3%n6!1w$h*6y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','music-rec.herokuapp.com']
 
