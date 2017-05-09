3c0b20d0a14afdaa3da67a0f7a3d286831fa3d65
tlss94 <doantl89@gmail.com>
2017-05-09 17:47:30 -0400

    f

--- maquette/settings.py
+++ maquette/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 #ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','https://music-rec.herokuapp.com']
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost','music-rec.herokuapp.com']
-ALLOWED_HOSTS = ['localhost','music-rec.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 ADMINS = [('thang', 'doantl89@gmail.com'),]
 
